fdcf93a53eed480fcf7e045fec85cdd9efbc5036
Suraj Molke <163307027+Smolke9@users.noreply.github.com>
2025-09-08 11:35:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Dockerized Node.js App 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-module.exports = app; // Export for testing
-
+module.exports = app;
